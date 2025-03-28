0d9b7f7c05011d11eff22f983434bd7275b1327b
Sanskar Jethi <sansyrox@gmail.com>
2025-03-28 01:43:37 -0700

    update

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -1136,8 +1136,8 @@ function SettingsContent() {
                     />
                   </div>
 
-                  <div className="flex gap-4 hidden">
-                    <Card className="bg-black border-zinc-800 flex-1">
+                  <div className="flex gap-4">
+                    <Card className="hidden bg-black border-zinc-800 flex-1">
                       <CardContent className="p-4">
                         <div className="flex items-center justify-between">
                           <div className="flex items-center gap-4">
